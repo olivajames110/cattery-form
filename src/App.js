d91fd107678b2fa23e4cf9f5b29e6c6805029146
Jimmy Oliva <olivajames110@gmail.com>
2020-02-13 21:51:14 -0500

    Form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header" />
-		</div>
-	);
+    return (
+        <div className="App">
+            <header className="app-header" />
+            <div className="form">Form</div>
+        </div>
+    );
 }
 
 export default App;
